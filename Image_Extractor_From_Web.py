6122f9352d83f86d2f9e82da8e364ad32c677d98
chris <chrisrossell1999@gmail.com>
2023-09-06 15:08:42 +0100

    Altered the image extractor script to be a function for better modularity

--- Image_Extractor_From_Web.py
+++ Image_Extractor_From_Web.py
@@ -1,13 +1,30 @@
 import requests
 
+
+def image_download(image_url, image_name):
+    """This function takes an image url from the web and
+    creates a jpg file in the local directory with the inputted name
+    the url needs to be the exact url of the image
+    the image_name must be compatible with naming of files"""
+    response = requests.get(image_url)
+    with open(f"{image_name}.jpg", "wb") as file:
+        file.write(response.content)
+
+
 url = "https://upload.wikimedia.org/wikipedia/commons" \
       "/thumb/9/98/Aldrin_Apollo_11_original" \
       ".jpg/390px-Aldrin_Apollo_11_original.jpg"
-response = requests.get(url)
 
-# if response 200 then successful extraction
-# into the response variable
-print(response)
+image_download(image_url=url, image_name="image_extracted_function")
 
-with open("image_extracted.jpg", "wb") as file:
-    file.write(response.content)
+# url = "https://upload.wikimedia.org/wikipedia/commons" \
+#       "/thumb/9/98/Aldrin_Apollo_11_original" \
+#       ".jpg/390px-Aldrin_Apollo_11_original.jpg"
+# response = requests.get(url)
+#
+# # if response 200 then successful extraction
+# # into the response variable
+# print(response)
+#
+# with open("image_extracted.jpg", "wb") as file:
+#     file.write(response.content)
